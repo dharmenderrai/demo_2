34b8f43138b29f78fdf641d4e0f86c7a23cea945
Dharmender Rai <dharmender.rai@gmail.com>
2023-07-20 07:26:18 +0800

    commit

---
+++ js/foreach.js
@@ -0,0 +1,21 @@
+// const count_non_Zeros_var = function (arr){
+//     let total = 0;
+//     arr.forEach(function(val){
+//       if(val != 0) {
+//           total++;
+//       }
+//     });
+//     return total;
+//   }
+  
+//   console.log(count_non_Zeros_var([1, 0, 0, 1, 2, 3]))
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+function squareNumber(number) {
+  return number * number;
+}
+
+console.log(numbers);
+console.log(numbers.map(squareNumber));
